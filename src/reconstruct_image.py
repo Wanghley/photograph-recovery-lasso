5825d209375907ef5f1a5914a853fe79d7c13191
Wanghley Soares Martins <wanghleys@gmail.com>
2025-03-07 07:24:08 -0500

    refactor: streamline image reconstruction process and suppress warnings

--- src/reconstruct_image.py
+++ src/reconstruct_image.py
@@ -4,6 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.linear_model import Lasso
 from tqdm import tqdm
+import warnings
 
 # Add path to src to sys
 sys.path.append(os.path.join(os.getcwd(), '../src'))
@@ -11,7 +12,6 @@ sys.path.append(os.path.join(os.getcwd(), '../src'))
 from blockfy import Blockfy
 from basisgen import BasisGenerator
 from lassoalphaoptimizer import LassoAlphaOptimizer
-import warnings
 
 class ImageReconstructor:
     def __init__(self, P, Q, S, M, train_test_ratio, img_path=None, corrupted_img=None):
@@ -24,65 +24,65 @@ class ImageReconstructor:
         self.train_test_ratio = train_test_ratio
 
     def reconstruct_image(self):
-        # Initialize Blockfy
-        if self.img_path:
-            blockfy = Blockfy(self.img_path, (self.P, self.Q), self.P)
-        else:
-            blockfy = Blockfy(self.corrupted_img, (self.P, self.Q), self.P)
-        
-        blockfy.generate_blocks()
-        corrpt_blocks = self.corrupted_img if self.corrupted_img else blockfy.generate_corrupted_blocks(self.S)
-        original_blocks = blockfy.get_blocks()
-        blocks_positioning = blockfy.get_blocks_positioning()
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
 
-        # Optimize alphas
-        lassoalphaoptimizer = LassoAlphaOptimizer(self.S, self.M, self.train_test_ratio, self.P, self.Q)
-        if self.img_path:
-            alphas = lassoalphaoptimizer.optimize_alphas(self.img_path)
-        else:
-            alphas = lassoalphaoptimizer.optimize_alphas(self.corrupted_img, is_corrupted=True)
+            # Initialize Blockfy
+            if self.img_path:
+                blockfy = Blockfy(self.img_path, (self.P, self.Q), self.P)
+            else:
+                blockfy = Blockfy(self.corrupted_img, (self.P, self.Q), self.P)
 
-        # Generate basis matrix
-        basisgen = BasisGenerator(self.P, self.Q)
-        basis_matrix = basisgen.generate_basis_matrix()
+            blockfy.generate_blocks()
+            corrpt_blocks = self.corrupted_img if self.corrupted_img else blockfy.generate_corrupted_blocks(self.S)
+            original_blocks = blockfy.get_blocks()
+            blocks_positioning = blockfy.get_blocks_positioning()
 
-        # Reconstruct blocks
-        recovered_blocks = []
-        for i in tqdm(range(min(len(corrpt_blocks), len(alphas)))):
-            block = corrpt_blocks[i]
-            alpha = alphas[i]
-            blk_flat = block.flatten()
-            good_idx = np.where(~np.isnan(blk_flat))[0].astype(int)
-            corrpt_idx = np.isnan(blk_flat)
-            X = basis_matrix[good_idx, :]
-            y = blk_flat[good_idx]
-            # Suppress convergence warnings
-            with warnings.catch_warnings():
-                warnings.simplefilter("ignore")
-                lasso = Lasso(alpha=alpha, fit_intercept=False, max_iter=10000)
+            # Optimize alphas
+            lassoalphaoptimizer = LassoAlphaOptimizer(self.S, self.M, self.train_test_ratio, self.P, self.Q)
+            if self.img_path:
+                alphas = lassoalphaoptimizer.optimize_alphas(self.img_path)
+            else:
+                alphas = lassoalphaoptimizer.optimize_alphas(self.corrupted_img, is_corrupted=True)
+
+            # Generate basis matrix
+            basisgen = BasisGenerator(self.P, self.Q)
+            basis_matrix = basisgen.generate_basis_matrix()
+
+            # Reconstruct blocks
+            recovered_blocks = []
+            for i in tqdm(range(min(len(corrpt_blocks), len(alphas)))):
+                block = corrpt_blocks[i]
+                alpha = alphas[i]
+                blk_flat = block.flatten()
+                good_idx = np.where(~np.isnan(blk_flat))[0].astype(int)
+                corrpt_idx = np.isnan(blk_flat)
+                X = basis_matrix[good_idx, :]
+                y = blk_flat[good_idx]
+                lasso = Lasso(alpha=alpha, fit_intercept=False)
                 lasso.fit(X, y)
-            recov_chip = np.zeros((self.P, self.Q))
-            full_block = corrpt_blocks[i].copy()
-            block_flat = full_block.flatten()
-            nan_indices = np.where(np.isnan(block_flat))[0]
-            X_missing = basis_matrix[nan_indices]
-            y_missing = lasso.predict(X_missing)
-            block_flat[nan_indices] = y_missing
-            recov_chip = block_flat.reshape(self.P, self.Q)
-            recovered_blocks.append(recov_chip)
+                recov_chip = np.zeros((self.P, self.Q))
+                full_block = corrpt_blocks[i].copy()
+                block_flat = full_block.flatten()
+                nan_indices = np.where(np.isnan(block_flat))[0]
+                X_missing = basis_matrix[nan_indices]
+                y_missing = lasso.predict(X_missing)
+                block_flat[nan_indices] = y_missing
+                recov_chip = block_flat.reshape(self.P, self.Q)
+                recovered_blocks.append(recov_chip)
 
-        # Convert recovered blocks to numpy array
-        recovered_blocks_array = np.array(recovered_blocks)
+            # Convert recovered blocks to numpy array
+            recovered_blocks_array = np.array(recovered_blocks)
 
-        # Reconstruct full images
-        original = None if self.corrupted_img else blockfy.block_to_image(original_blocks)
-        corrupted = self.corrupted_img if self.corrupted_img else blockfy.block_to_image(corrpt_blocks)
-        recovered_img = blockfy.block_to_image(recovered_blocks_array)
-        
-        # save images to class
-        self.original_img = original
-        self.corrupted_img = corrupted
-        self.recovered_img = recovered_img
+            # Reconstruct full images
+            original = None if self.corrupted_img else blockfy.block_to_image(original_blocks)
+            corrupted = self.corrupted_img if self.corrupted_img else blockfy.block_to_image(corrpt_blocks)
+            recovered_img = blockfy.block_to_image(recovered_blocks_array)
+
+            # save images to class
+            self.original_img = original
+            self.corrupted_img = corrupted
+            self.recovered_img = recovered_img
 
     def plot_images(self):
         plt.figure(figsize=(30, 20))
@@ -93,21 +93,24 @@ class ImageReconstructor:
             plt.imshow(self.corrupted_img, cmap=cmap)
             plt.title('Corrupted Image')
             plt.axis('off')
-        
+
         if self.recovered_img is not None:
             plt.subplot(1, 3, 2)
             plt.imshow(self.recovered_img, cmap='gray')
             plt.title('Recovered Image')
             plt.axis('off')
-        
+
         if self.original_img is not None:
             plt.subplot(1, 3, 3)
             plt.imshow(self.original_img, cmap='gray')
             plt.title('Original Image')
             plt.axis('off')
-        
+
         plt.tight_layout()
         plt.show()
+
+    def get_images(self):
+        return self.original_img, self.corrupted_img, self.recovered_img
 
 # if __name__ == "__main__":
 #     img_path = '../assets/nature/nature.bmp'
